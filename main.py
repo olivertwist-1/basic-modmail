5d1117c576c97212ae61f2adce67020dae77bb3d
Juan Sebastián <89955811+juansebastian2006@users.noreply.github.com>
2021-12-01 21:45:10 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import json
+from discord.ext.commands import MissingPermissions
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix="your_prefix", help_command=None, intents=intents.all())
+
+cogs = ['modmail']
+
+for c in cogs:
+    bot.load_extension(c)
+
+
+@bot.event
+async def on_ready():
+    print("BOT READY")
+
+
+@bot.command()
+@commands.has_permissions(manage_channels=True, manage_messages=True)
+async def get_data(ctx):
+    with open("ModMail_file.json", "r") as f:
+        data = json.load(f)
+    json_ = json.dumps(data, indent=4)
+
+    await ctx.send(f"```json\n{json_}```")
+
+
+@get_data.error
+async def get_data_error(ctx, error):
+    if isinstance(error, MissingPermissions):
+        await ctx.send("You're not allowed to use this", delete_after=5)
+    else:
+        raise error
+
+
+
+bot.run('TOKEN')
